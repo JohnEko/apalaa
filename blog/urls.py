1ab52c35f4be363254ec50c0e16c3d5b56f5c85a
efezino10@gmail.com <efezino10@gmail.com>
2024-11-24 13:08:24 +0000

    New formView update

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('news/<str:pk>/', views.news, name='news'),
+    path('create_post/', views.createPost, name='create'),
 
 ]
