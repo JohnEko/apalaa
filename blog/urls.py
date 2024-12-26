aa2d0cd4c4668f8af61c46832ff4cb3635c4309f
efezino10@gmail.com <efezino10@gmail.com>
2024-12-26 18:47:28 +0000

    New permission for user added and login and logout function

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
     path('', views.home, name='home'),
     path('news/<str:pk>/', views.news, name='news'),
     path('create-post/', views.createPost, name='create'),
